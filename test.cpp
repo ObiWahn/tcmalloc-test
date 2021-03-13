1ebe23e9696a840d1346bfff1595b7543b54d92e
Jan Christoph Uhde <Jan@UhdeJC.com>
2021-03-13 19:44:30 +0100

    Add a hint how to use the `gperftools` version of `tcmalloc`.

--- test.cpp
+++ test.cpp
@@ -13,17 +13,17 @@
 int main() {
     std::vector<std::vector<int>> res;
 
-    for(std::size_t i = 0; i<= 10000000; i++){
+    for(std::size_t i = 0; i<= 10000000; i++) {
         res.push_back(std::vector<int>(100));
     }
     res.clear();
 
-    for(std::size_t i = 0; i<= 1000000; i++){
+    for(std::size_t i = 0; i<= 1000000; i++) {
         res.push_back(std::vector<int>(1000));
     }
     res.clear();
 
-    for(std::size_t i = 0; i<= 100000; i++){
+    for(std::size_t i = 0; i<= 100000; i++) {
         res.push_back(std::vector<int>(10000));
     }
     res.clear();
@@ -37,6 +37,14 @@ int main() {
     MallocExtension::instance()->GetHeapSample(&data);
     std::ofstream out("./stats.dump2");
     out << data;
+
+    std::cout << "To make this work you need to:\n"
+              << "  export TCMALLOC_SAMPLE_PARAMETER=524288 (or some other value)\n"
+              << "Then you can inspect the dump with:\n"
+              << "  pprof -http localhost:5000 stats.dump2\n"
+              << "after running this executable";
+
 #endif
+    std::cout << "\n";
     return 0;
 }
